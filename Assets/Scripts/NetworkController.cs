510eed8fe70580d0e357706909c4dc487b2cddfc
Steven <79434354+yeslawr2@users.noreply.github.com>
2022-03-29 18:43:31 -0500

    Initialized Multiplayer Network

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        //base.OnConnectedToMaster();
+        Debug.Log("Now connected to the " + PhotonNetwork.CloudRegion + " server");
+    }
+}
