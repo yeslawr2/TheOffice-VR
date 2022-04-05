92f5a8417f31bb8a96aac9d31f4ae9aa81f2b4bb
Steven <79434354+yeslawr2@users.noreply.github.com>
2022-04-04 19:27:42 -0500

    Added Player Movement and ProBuilder
    Create a conference room using the newly installed ProBuilder

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private CharacterController characterControllerComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterControllerComponent = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        Vector3 velocity = direction * 2.0f;
+        velocity = Camera.main.transform.TransformDirection(velocity);
+        velocity.y -= 10.0f;
+        characterControllerComponent.Move(velocity * Time.deltaTime);
+    }
+}
