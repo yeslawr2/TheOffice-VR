457b583ff53f39fabd7616af61a73f7eb8ec34af
Steven <79434354+yeslawr2@users.noreply.github.com>
2022-04-11 17:36:27 -0500

    Added on/off functionality to spawn room doors

---
+++ Assets/Scripts/SpawnRoomDoor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRoomDoor : MonoBehaviour
+{
+    [SerializeField] private Canvas enterRoomMenu;
+
+    private Outline outlineComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        outlineComponent = GetComponent<Outline>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (outlineComponent.enabled)
+        {
+            if (Input.GetKeyDown(KeyCode.K) || Input.GetButtonDown("js0"))
+            {
+                enterRoomMenu.gameObject.SetActive(!enterRoomMenu.gameObject.activeInHierarchy);
+            }
+        }
+    }
+}
